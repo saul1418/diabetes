f17ecfafb75e634e2119d97b1bda1d0edd45176b
saul1418 <saulserrano186@gmail.com>
2025-07-31 15:58:53 -0500

    Fix: agregar Conv a add_safe_globals para cargar modelo YOLO

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ from fastapi.responses import JSONResponse
 from ultralytics import YOLO
 import cv2
 import numpy as np
-import torch
-from ultralytics.nn.tasks import DetectionModel
-from ultralytics.nn.modules.conv import Conv
-import torch.nn.modules.container
-
-safe_globals = [
-    DetectionModel,
-    torch.nn.modules.container.Sequential,
-    Conv,
-]
-
-torch.serialization.add_safe_globals(safe_globals)
 
 app = FastAPI()
 model = YOLO("best.pt")  # Cambia por la ruta real de tu modelo si es necesario
@@ -31,7 +19,7 @@ async def predict(file: UploadFile = File(...)):
         detections.append({
             "class": results[0].names[int(box.cls[0])],
             "confidence": float(box.conf[0]),
-             "box": [x1, y1, x2, y2]
+            "box": [x1, y1, x2, y2]
         })
     return JSONResponse(detections)
 
